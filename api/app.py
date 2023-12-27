4610fe8c0c7c9c50694ff61ca20d56ea0cbcc1fa
Juanchi <52383717+juanchixd@users.noreply.github.com>
2023-12-26 22:11:23 -0300

    change uptimerobot to request status

--- api/app.py
+++ api/app.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template, redirect, url_for
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import requests
+import threading
 app = Flask(__name__)
 
-UPTIME_ROBOT_API_KEY = os.environ.get("UPTIME_ROBOT_API_KEY")
 REDIRECT_URL = os.environ.get("REDIRECT_URL")
-url = "https://api.uptimerobot.com/v2/getMonitors"
+CHECK_INTERVAL = timedelta(minutes=os.environ.get("CHECK_INTERVAL"))
 
-payload = f"api_key={UPTIME_ROBOT_API_KEY}&format=json&logs=1&response_times=1"
-headers = {
-    'content-type': "application/x-www-form-urlencoded",
-    'cache-control': "no-cache"
-}
+server_online = True
 
 
 def check_server_status():
+    global server_online
     try:
-        response = requests.request("POST", url, data=payload, headers=headers)
-        data = response.json()
-        return data["monitors"][0]["status"] == 2
-    except:
-        return False
+        response = requests.get(REDIRECT_URL)
+        server_online = response.status_code == 200
+    except Exception as e:
+        print(f"Error checking server status: {e}")
+        server_online = False
+
+
+def schedule_server_check():
+    threading.Timer(CHECK_INTERVAL.total_seconds(),
+                    schedule_server_check).start()
+    check_server_status()
+
+
+def schedule_server_check():
+    threading.Timer(CHECK_INTERVAL.total_seconds(),
+                    schedule_server_check).start()
+    check_server_status()
+
+
+schedule_server_check()
 
 
 @app.route('/')
